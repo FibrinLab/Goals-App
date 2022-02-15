f8b3045db43ee84e0bc33c2a77ebbe61ca7caaa8
FibrinLab <fibrinlab@gmail.com>
2022-02-15 11:50:05 +0100

    Controller and Router done

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,37 @@
+const asyncHandler = require('express-async-handler')
+
+const getGoals = asyncHandler(async (req, res) => {
+    res.status(200).json({
+        message: "Get Goals"
+    })
+})
+
+const setGoals = asyncHandler(async (req, res) => {
+    if (!req.body.text) {
+        res.status(400)
+        throw new Error('Please add text')
+    }
+
+    res.status(200).json({
+        message: "Set Goals"
+    })
+})
+
+const updateGoals = asyncHandler(async (req, res) => {
+    res.status(200).json({
+        message: `Update Goals ${req.params.id}`
+    })
+})
+
+const deleteGoals = asyncHandler(async (req, res) => {
+    res.status(200).json({
+        message: `Delete Goals ${req.params.id}`
+    })
+})
+
+module.exports = {
+    getGoals,
+    setGoals,
+    updateGoals,
+    deleteGoals
+}
